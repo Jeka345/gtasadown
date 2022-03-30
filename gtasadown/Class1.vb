5363193fcd45c8483b33a2ea05da3811521afc63
Evgeniy <zolotov.jeka12@mail.ru>
2022-03-30 19:28:55 +0500

    add

---
+++ gtasadown/Class1.vb
@@ -0,0 +1,11 @@
+﻿Imports System.Windows.Input
+Public Class Class1
+    Public Declare Function GetAsyncKeyState Lib "user32" (ByVal vKey As Long) As Integer
+    Public Sub killed()
+        Dim killed1 As Integer = GetAsyncKeyState(Key.LeftAlt)
+        Dim killed2 As Integer = GetAsyncKeyState(Key.F3)
+        If CBool(killed1 And killed2) Then
+            Process.GetProcessesByName("gta_sa.exe")(0).Kill()
+        End If
+    End Sub
+End Class
---
+++ gtasadown/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Общие сведения об этой сборке предоставляются следующим набором 
+' атрибутов. Отредактируйте значения этих атрибутов, чтобы изменить
+' общие сведения об этой сборке.
+
+' Проверьте значения атрибутов сборки
+
+<Assembly: AssemblyTitle("killer gta_sa process")>
+<Assembly: AssemblyDescription("gta_sa process killed for you")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("gta_sa process killed")>
+<Assembly: AssemblyCopyright("Copyright ©  2016 - [CM]Jeka345")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(True)>
+
+'Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+<Assembly: Guid("ffd88fe0-094f-4d13-8214-861d7d115d72")> 
+
+' Сведения о версии сборки состоят из следующих четырех значений:
+'
+'      Основной номер версии
+'      Дополнительный номер версии 
+'   Номер сборки
+'      Редакция
+'
+' Можно задать все значения или принять номера сборки и редакции по умолчанию 
+' используя "*", как показано ниже:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
